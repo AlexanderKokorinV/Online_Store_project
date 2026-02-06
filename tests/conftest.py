7b1b63317a6bb5553c4ee598d73033cd549ecc06
Alexander <sanya.core@gmail.com>
2026-02-06 18:42:06 +0300

    classes and utils modules have been added

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from src.classes import Product, Category
+
+@pytest.fixture
+def product_iphone():
+    """Фикстура для тестирования инициализации товара iPhone"""
+    return Product("iPhone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product_samsung():
+    """Фикстура для тестирования инициализации товара Samsung"""
+    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture(autouse=True)
+def clean_category_counters():
+    """Автоматически очищает счетчики перед каждым тестом"""
+    Category.category_count = 0
+    Category.product_count = 0
